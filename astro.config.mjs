6d83da5c70607952b1b7434230279f9b7a6386a3
Carles Serna <carles.serna@gmail.com>
2024-03-12 17:30:09 +0100

    fix blog page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
-import compress from 'astro-compress';
-import icon from "astro-icon";
-
-import netlify from "@astrojs/netlify";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
+import compress from 'astro-compress'
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [mdx(), icon(), tailwind({
-    applyBaseStyles: false
-  }), compress()],
-  output: "server",
-  adapter: netlify()
-});
+  integrations: [
+    mdx(),
+    icon(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    compress(),
+  ],
+})
